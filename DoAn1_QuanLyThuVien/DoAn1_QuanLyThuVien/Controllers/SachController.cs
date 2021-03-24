f8b22cd0db7f6eb82585704f249b3038100b19e7
quocdai03032000 <quocdai03032000@gmail.com>
2021-03-24 21:37:44 +0700

    Hoan thanh chu nang them,xoa,sua BOOK

--- DoAn1_QuanLyThuVien/DoAn1_QuanLyThuVien/Controllers/SachController.cs
+++ DoAn1_QuanLyThuVien/DoAn1_QuanLyThuVien/Controllers/SachController.cs
@@ -33,8 +33,36 @@ namespace DoAn1_QuanLyThuVien.Controllers
         public ActionResult Details(int id)
         {
             return View(database.Saches.Where(a => a.MaSach == id).FirstOrDefault());
-           
-            
+                       
+        }
+
+        //Edit sách
+        public ActionResult Edit(int id)
+        {
+            return View(database.Saches.Where(a=>a.MaSach==id).FirstOrDefault());
+        }
+        [HttpPost]
+        public ActionResult Edit(int id, Sach sach)
+        {
+            database.Entry(sach).State = System.Data.Entity.EntityState.Modified;
+            database.SaveChanges();
+            return Redirect("Index");
+        }
+
+        //Xoá sách
+
+        public ActionResult Delete(int id)
+        {
+            return View(database.Saches.Where(a => a.MaSach == id).FirstOrDefault());
+        }
+
+        [HttpPost]
+        public ActionResult Delete(int id, Sach sach)
+        {
+            sach = database.Saches.Where(a => a.MaSach == id).FirstOrDefault();
+            database.Saches.Remove(sach);
+            database.SaveChanges();
+            return RedirectToAction("Index");
         }
     }
 }
