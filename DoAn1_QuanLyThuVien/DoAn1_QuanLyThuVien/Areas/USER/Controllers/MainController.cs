df02bfbb7d60cd6b52743255575c79a7bc0422db
quocdai03032000 <quocdai03032000@gmail.com>
2021-04-20 22:14:40 +0700

    update project DoAn1

---
+++ DoAn1_QuanLyThuVien/DoAn1_QuanLyThuVien/Areas/USER/Controllers/MainController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DoAn1_QuanLyThuVien.Areas.USER.Controllers
+{
+    public class MainController : Controller
+    {
+        // GET: USER/Main
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+
+
+        /*****************/
+        /***** Login *****/
+        /*****************/
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        /*****************/
+        /***** Register***/
+        /*****************/
+        public ActionResult Register()
+        {
+            return View();
+        }
+
+        /*****************/
+        /***** Contact ***/
+        /*****************/
+        public ActionResult Contact()
+        {
+            return View();
+        }
+    }
+}
